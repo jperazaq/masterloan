545fe6e261123e940aaa55a8c851bd9f84675a0c
Julio Peraza <julioperaza@gmail.com>
2020-10-20 20:05:30 -0600

    martes

--- conexion.php
+++ conexion.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 $servername = "localHost";
 $database = "masterlend";
@@ -12,19 +12,7 @@ if (!$conn) {
 }
 // // echo "Connected successfully";
 //  mysqli_close($conn);
-=======
-<?php
-$servername = "localHost";
-$database = "masterlend";
-$username = "root";
-$password = "";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-// // echo "Connected successfully";
-//  mysqli_close($conn);
->>>>>>> e90219b47302d82c4d9aa683e2636f89eae7be42
+
+
+
 ?>
