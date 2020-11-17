80b500f850e884bd9bb9df89db38864a2447ecc8
Julio Peraza <julioperaza@gmail.com>
2020-11-16 19:31:52 -0600

    segundo

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 
 <?php
-$servername = "localHost";
-$database = "masterlend";
-$username = "root";
-$password = "";
+$servername = "us-cdbr-east-02.cleardb.com";
+$database = "heroku_07d992e5526fdc5";
+$username = "b9321a99880982";
+$password = "bdaa56d9";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
@@ -16,3 +16,5 @@ if (!$conn) {
 
 
 ?>
+
+<!-- mysql://b9321a99880982:bdaa56d9@us-cdbr-east-02.cleardb.com/heroku_07d992e5526fdc5?reconnect=true -->
