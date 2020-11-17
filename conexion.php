790dda0493bb78eca131e07845a1c28280d908fb
Julio Peraza <julioperaza@gmail.com>
2020-11-16 20:50:59 -0600

    julio

--- conexion.php
+++ conexion.php
@@ -1,11 +1,13 @@
 
 <?php
-$servername = "us-cdbr-east-02.cleardb.com";
-$database = "heroku_07d992e5526fdc5";
 $username = "b9321a99880982";
 $password = "bdaa56d9";
+$servername = "us-cdbr-east-02.cleardb.com";
+$database = "heroku_07d992e5526fdc5";
+
+
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
+$conn = mysqli_connect( $username, $password, $servername, $database);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error($conn));
