f30cfa2762a50ad4abea8510e7cf0542c090c280
Julio Peraza <julioperaza@gmail.com>
2020-11-16 20:43:15 -0600

    hola

--- conexion.php
+++ conexion.php
@@ -8,7 +8,7 @@ $password = "bdaa56d9";
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error($conn));
 }
 // // echo "Connected successfully";
 //  mysqli_close($conn);
